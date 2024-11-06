0643d7c85a82320d0250692101e7877ea13f6360
RickyMambu <rickymambu14@gmail.com>
2024-11-06 09:53:56 +0800

    Excercise HTML to React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Clients from "./component/Clients";
+import Features from "./component/Features";
+import Footer from "./component/Footer";
+import Header from "./component/header";
+import Hero from "./component/Hero";
+import Pricing from "./component/Pricing";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body-wrap">
+      <Header />
+      <main className="site-content">
+        <Hero />
+        <Clients />
+        <Features />
+        <Pricing />
+      </main>
+      <Footer />
     </div>
+
   );
 }
 
